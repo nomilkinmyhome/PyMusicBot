8a0c47096dca9139a4a1f3bdc528d96500366966
akernelpanicv <vyckluchi75@gmail.com>
2020-03-19 09:31:49 +0300

    bugfix

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,6 @@
-if ! [ -d migrations/ ]; then
-  PyMusicBot_SETTINGS=settings.cfg python3.8 manage.py db init
+if ! [ -d migrations/ ]
+  then PyMusicBot_SETTINGS=settings.cfg python3.8 manage.py db init
+else
+  PyMusicBot_SETTINGS=settings.cfg python3.8 manage.py db migrate
+  PyMusicBot_SETTINGS=settings.cfg python3.8 manage.py db upgrade
 fi
-
-PyMusicBot_SETTINGS=settings.cfg python3.8 manage.py db migrate
-PyMusicBot_SETTINGS=settings.cfg python3.8 manage.py db upgrade
