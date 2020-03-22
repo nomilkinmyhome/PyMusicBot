70c1e352e2e911dd4129d33d39d6bd50b84371ad
akernelpanicv <vyckluchi75@gmail.com>
2020-03-22 15:32:32 +0300

    add typing

--- utils.py
+++ utils.py
@@ -2,33 +2,40 @@ import re
 
 from app import db
 from models import Music
-from sqlalchemy.exc import SQLAlchemyError
 
+from sqlalchemy.exc import SQLAlchemyError
 from werkzeug.utils import secure_filename, escape
 
 
 class SecureMusicSave:
-    def __init__(self, **kwargs):
+    """Class for secure saving music files to the DB and media/ directory
+
+    :param **kwargs: must contain 'music_title', 'music_file' and 'url_root'
+    """
+    def __init__(self, **kwargs) -> None:
         self.music_title = kwargs['music_title']
         self.music_file = kwargs['music_file']
 
-        self.media_root = 'media'
-        self.media_root_url = f'{kwargs["url_root"]}/{self.media_root}'
+        self.media_root: str = 'media'
+        self.media_root_url: str = f'{kwargs["url_root"]}/{self.media_root}'
 
         self.__get_clean_music_title()
+        self.__get_correct_music_title()
 
-    def __get_clean_music_title(self):
+    def __get_clean_music_title(self) -> None:
+        self.music_title: str = escape(self.music_title)
+        self.music_title: str = secure_filename(self.music_title)
+
+    def __get_correct_music_title(self) -> None:
         if not re.search(r'^.+\.mp3$', self.music_title):
-            self.music_title = f'{self.music_title}.mp3'
-        self.music_title = escape(self.music_title)
-        self.music_title = secure_filename(self.music_title)
+            self.music_title: str = f'{self.music_title}.mp3'
 
-    def save_to_dir(self):
+    def save_to_dir(self) -> bool:
         self.music_file.save(f'{self.media_root}/{self.music_title}')
 
         return True
 
-    def save_to_db(self):
+    def save_to_db(self) -> bool:
         try:
             music = Music(title=self.music_title, url=f'{self.media_root_url}{self.music_title}')
             db.session.add(music)
