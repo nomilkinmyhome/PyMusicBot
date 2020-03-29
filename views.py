e97bc19d26fc3cdf927e400c304d7d604a9f20fc
akernelpanicv <vyckluchi75@gmail.com>
2020-03-29 18:54:25 +0300

    refactoring

--- views.py
+++ views.py
@@ -5,82 +5,85 @@ from utils import SecureMusicCRUD
 from flask import render_template, request, redirect, url_for
 from flask_login import current_user, login_user, login_required, logout_user
 from flask.views import View, MethodView
+
 from werkzeug.exceptions import BadRequestKeyError
+from werkzeug.datastructures import FileStorage
+from flask_sqlalchemy import Pagination, BaseQuery
+from typing import Union, Dict
 
 
 class BasePage(MethodView):
-    decorators = [login_required]
-    context_objects = {}
+    """Interface for all pages of the site"""
+    decorators: list = [login_required]
 
-    def get(self):
-        self.context_objects['page_title'] = 'Admin Page'
-        return self.render_template(self.context_objects)
+    template: str = 'admin.html'
 
-    def post(self):
+    def get(self) -> Union[redirect, render_template]:
+        context: dict = self.get_context()
+        if 'page_title' not in context:
+            context['page_title'] = 'Admin Page'
+
+        return self.render_template(context)
+
+    def post(self) -> Union[None, redirect]:
         pass
 
-    def render_template(self, context):
-        return render_template(self.get_template_name(), **context)
+    def render_template(self, context) -> render_template:
+        return render_template(self.template, **context)
 
-    def get_template_name(self):
-        return 'admin.html'
+    def get_context(self) -> dict:
+        pass
 
 
 class Auth(BasePage):
-    decorators = []
-    context_objects = {'page_title': 'Log in',
-                       'auth_form': AuthForm()}
+    """Auth page"""
+    decorators: list = []
 
-    def get(self):
+    template: str = 'auth.html'
+
+    def get(self) -> Union[redirect, render_template]:
         if current_user.is_authenticated:
             return redirect(url_for('admin_music_list'))
 
-        return self.render_template(self.context_objects)
+        return self.render_template(self.get_context())
 
-    def post(self):
+    def post(self) -> redirect:
         user = User.query.filter_by(name=request.form['login']).first()
         if user is not None and user.check_password(request.form['password']):
             login_user(user, remember=True)
 
             return redirect(url_for('admin_music_list'))
 
-    def get_template_name(self):
-        return 'auth.html'
+    def get_context(self) -> Dict[str, Union[str, AuthForm]]:
+        context: Dict[str, Union[str, AuthForm]] = {'page_title': 'Log in',
+                                                    'auth_form': AuthForm()}
+
+        return context
 
 
 class Logout(View):
-    decorators = [login_required]
+    decorators: list = [login_required]
 
-    def dispatch_request(self):
+    def dispatch_request(self) -> redirect:
         logout_user()
 
         return redirect(url_for('auth'))
 
 
-class MusicList(View):
-    decorators = [login_required]
-
-    def dispatch_request(self):
-        content_title = 'Music list'
-
-        context = {'page_title': 'Admin Page',
-                   'content_title': content_title,
-                   'search': self.get_search(),
-                   'pages': self.get_pages()}
-
-        return self.render_template(context)
-
-    def get_search(self):
+class MusicList(BasePage):
+    """Music list page"""
+    def get_search(self) -> Union[None, str]:
         return request.args.get('search')
 
-    def get_pages(self):
-        current_page = self.get_current_page()
-        music_list = self.get_music_list()
+    def get_pages(self) -> Pagination:
+        current_page: int = self.get_current_page()
+        music_list: BaseQuery = self.get_music_list()
 
         return music_list.paginate(page=current_page, per_page=10)
 
-    def get_current_page(self):
-        current_page = request.args.get('page')
+    @staticmethod
+    def get_current_page() -> int:
+        current_page: Union[int, str, None] = request.args.get('page')
 
         if current_page and current_page.isdigit():
             current_page = int(current_page)
@@ -89,25 +92,27 @@ class MusicList(View):
 
         return current_page
 
-    def get_music_list(self):
-        search = self.get_search()
+    def get_music_list(self) -> BaseQuery:
+        search: Union[None, str] = self.get_search()
         if search:
             return Music.query.filter(Music.title.ilike(f'%{search}%'))
         else:
             return Music.query.order_by(Music.pub_date.desc())
 
-    def render_template(self, context):
-        return render_template('admin.html', **context)
+    def get_context(self) -> Dict[str, Union[str, Pagination, None]]:
+        context: Dict[str, Union[str, Pagination, None]] = {'content_title': 'Music list',
+                                                            'search': self.get_search(),
+                                                            'pages': self.get_pages()}
+
+        return context
 
 
 class AddMusic(BasePage):
-    context_objects = {'content_title': 'Add music',
-                       'add_music_form': AddMusicForm()}
-
-    def post(self):
+    """Music adding page"""
+    def post(self) -> redirect:
         try:
-            music_title = request.form['title']
-            music_file = request.files['music']
+            music_title: str = request.form['title']
+            music_file: FileStorage = request.files['music']
 
             secure_save = SecureMusicCRUD(**{'music_title': music_title,
                                              'url_root': request.url_root})
@@ -116,30 +121,44 @@ class AddMusic(BasePage):
         except BadRequestKeyError:
             pass
 
+    def get_context(self) -> Dict[str, Union[str, AddMusicForm]]:
+        context: Dict[str, Union[str, AddMusicForm]] = {'content_title': 'Add music',
+                                                        'add_music_form': AddMusicForm()}
+
+        return context
+
 
 class EditMusic(BasePage):
-    context_objects = {'content_title': 'Edit music',
-                       'edit_music_form': EditMusicForm()}
-
-    def post(self):
-        music_id = request.form['id']
-        music_title = request.form['title']
-        old_music_title = Music.query.filter(Music.id == music_id).first().title
+    """Music editing page"""
+    def post(self) -> redirect:
+        music_id: str = request.form['id']
+        music_title: str = request.form['title']
+        old_music_title: BaseQuery = Music.query.filter(Music.id == music_id).first().title
 
         secure_edit_music = SecureMusicCRUD(**{'music_title': music_title,
                                                'url_root': request.url_root})
         if secure_edit_music.rename_music_file_in_dir(old_music_title) and secure_edit_music.edit_music_in_db(music_id):
             return redirect(url_for('admin_music_list'))
 
+    def get_context(self) -> Dict[str, Union[str, EditMusicForm]]:
+        context: Dict[str, Union[str, EditMusicForm]] = {'content_title': 'Edit music',
+                                                         'edit_music_form': EditMusicForm()}
+
+        return context
+
 
 class DeleteMusic(BasePage):
-    context_objects = {'content_title': 'Delete music',
-                       'delete_music_form': DeleteMusicForm()}
-
-    def post(self):
-        music_id = request.form['id']
-        music_title = Music.query.filter(Music.id == music_id).first().title
+    """Music deletion page"""
+    def post(self) -> redirect:
+        music_id: str = request.form['id']
+        music_title: BaseQuery = Music.query.filter(Music.id == music_id).first().title
 
         secure_delete_music = SecureMusicCRUD(**{'music_title': music_title})
         if secure_delete_music.delete_music_file_from_dir() and secure_delete_music.delete_music_from_db(music_id):
             return redirect(url_for('admin_music_list'))
+
+    def get_context(self) -> Dict[str, Union[str, DeleteMusicForm]]:
+        context: Dict[str, Union[str, DeleteMusicForm]] = {'content_title': 'Delete music',
+                                                           'delete_music_form': DeleteMusicForm()}
+
+        return context
