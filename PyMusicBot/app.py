73647b28c50061fe97c7ecf15d108d33ac65da29
akernelpanicv <vyckluchi75@gmail.com>
2020-04-17 16:56:40 +0300

    added redis caching

--- PyMusicBot/app.py
+++ PyMusicBot/app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_login import LoginManager
+from flask_caching import Cache
 
 app = Flask(__name__)
 app.config.from_envvar('PyMusicBot_SETTINGS')
@@ -17,3 +18,8 @@ manager.add_command('db', MigrateCommand)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+cache = Cache(app, config={
+    'CACHE_TYPE': 'redis',
+    'CACHE_REDIS_URL': 'redis://localhost:6379/0',
+})
