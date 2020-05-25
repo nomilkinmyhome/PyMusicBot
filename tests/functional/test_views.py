a686ec4e01a54668c2b48361d3a3043909154053
akernelpanicv <vyckluchi75@gmail.com>
2020-05-25 15:53:06 +0300

    add authorization controller; add TestAdminPagesAccess

--- tests/functional/test_views.py
+++ tests/functional/test_views.py
@@ -1,12 +1,6 @@
-def login(client, login, password):
-    return client.post('/', data=dict(
-        login=login,
-        password=password
-    ), follow_redirects=True)
+import pytest
 
-
-def logout(client):
-    return client.get('/logout', follow_redirects=True)
+from .authorization_controller import AuthorizationController
 
 
 class TestAuthPage:
@@ -18,20 +12,40 @@ class TestAuthPage:
         assert b'PyMusicBot' in response.data
         assert b'Log in' in response.data
 
-    def test_correct_user_auth(self, test_client, init_database):
-        response = login(test_client, 'user1', 'very_bad_password')
-
-        assert response.status_code == 200
-
-        assert b'logout' in response.data
-        assert b'Admin Page' in response.data
-
-        logout(test_client)
-
     def test_incorrect_user_auth(self, test_client, init_database):
-        response = login(test_client, 'nonexistent_user', 'very_bad_password')
+        response = AuthorizationController.login(test_client, 'nonexistent_user', 'very_bad_password')
 
         assert response.status_code == 200
 
         assert b'Incorrect login or password!' in response.data
         assert b'Admin Page' not in response.data
+
+    def test_correct_user_auth(self, test_client, init_database):
+        response = AuthorizationController.login(test_client, 'user1', 'very_bad_password')
+
+        assert response.status_code == 200
+
+        assert b'logout' in response.data
+        assert b'Admin Page' in response.data
+        assert b'Incorrect login or password!' not in response.data
+
+
+class TestAdminPagesAccess:
+    _urls = ['/admin/list',
+             '/admin/add',
+             '/admin/edit',
+             '/admin/delete']
+
+    @pytest.mark.parametrize('url', _urls)
+    def test_authorized_access(self, test_client, init_database, url):
+        response = test_client.get(url)
+
+        assert response.status_code == 200
+
+    @pytest.mark.parametrize('url', _urls)
+    def test_unautorized_access(self, test_client, init_database, url):
+        AuthorizationController.logout(test_client)
+
+        response = test_client.get(url)
+
+        assert response.status_code == 401
